2fc77d635ee3e1c19187234ff97d50f95a76a51d
denna aliyene <aliounesidahmed@gmail.com>
2018-01-24 00:07:51 +0000

    menu enlever

--- app/src/main/java/denna/hassanaiafortourists/com/hassaniafortourists/activity/MainActivity.java
+++ app/src/main/java/denna/hassanaiafortourists/com/hassaniafortourists/activity/MainActivity.java
@@ -1,9 +1,9 @@
 package denna.hassanaiafortourists.com.hassaniafortourists.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -59,21 +59,7 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
+   
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
